e03f1f414807321f85e9af2d678e7512d75f1863
Natalia.Donchenko <n.donchenko1991@gmail.com>
2023-04-15 23:17:11 +0300

    1.4

--- src/App.js
+++ src/App.js
@@ -21,7 +21,10 @@ function Users () {
       {users?.map(user => (
         
           <li key={user.id}>
-            <Link to={`/user?userId=${user.id}`}>{user.name}</Link>
+            {user.name}
+            <Link to={`/user?userId=${user.id}`}>
+              <button>albums</button>
+            </Link>
           </li>
        
       ))}
@@ -33,29 +36,67 @@ function Users () {
 function Albums () {
   const [params] = useSearchParams()
   const id = params.get('userId')
-  const [album, setAlbum] = useState(0)
+  const [album, setAlbum] = useState({})
   const [albums, setAlbums] = useState([])
 
   console.log(album)
   console.log(id)
+  console.log(albums)
+
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/albums/${id}`);
+      const jsonData = await response.json();
+      setAlbum(jsonData);
+    }
+    fetchData();
+  }, []);
  
-
-  useEffect (() => {
-    fetch(`https://jsonplaceholder.typicode.com/albums/${id}`)
-      .then((res) => res.json())
-      .then(setAlbum)
-  }, [id])
-
-
-
+  if (id) {
+    
+  }
   return (
     
 
   <ul>
-      <li key={album.id}>{album?.title}</li>
+    
+      <li key={album.id}>
+        {album?.title}
+        <Link to={`/album?albumId=${album.id}`}>
+              <button>photos</button>
+            </Link>
+      </li>
 
     </ul>
 
+  )
+}
+
+function Photos () {
+
+  const [params] = useSearchParams()
+  const id = params.get('albumId')
+  const [photo, setPhoto] = useState()
+  console.log(photo)
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/photos/${id}`);
+      const jsonData = await response.json();
+      setPhoto(jsonData);
+    }
+    fetchData();
+  }, [id]);
+
+  return (
+    <ul>
+    
+        <li>
+          <img></img>
+        </li>
+
+    </ul>
   )
 }
 
@@ -65,6 +106,7 @@ function App () {
     <Routes>
       <Route path='/' element={<Users />}></Route>
       <Route path='/user' element={<Albums />}></Route>
+      <Route path='/album' element={<Photos/>}></Route>
     </Routes>
   )
 }
